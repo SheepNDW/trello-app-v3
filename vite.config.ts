8b40d30df56198a1a0144907c85f6e83239dd9ec
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-01-03 14:09:38 +0800

    chore: remove data-test attributes in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { URL, fileURLToPath } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+function removeDataTestAttrs(node: any) {
+  if (node.type === 1 /* NodeTypes.ELEMENT */) {
+    node.props = node.props.filter((prop: any) =>
+      prop.type === 6 /* NodeTypes.ATTRIBUTE */
+        ? prop.name !== 'data-test'
+        : true,
+    )
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        nodeTransforms: isProd ? [removeDataTestAttrs] : [],
+      },
+    },
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
