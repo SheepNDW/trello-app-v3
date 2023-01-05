d27c1f125c6b864187a531497391b3d6a91a5833
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-01-05 09:59:18 +0800

    feat: extract  EditBox component from BoardView

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -2,11 +2,11 @@
 import { computed, ref } from 'vue'
 import Card from '@/components/Card.vue'
 import { useStore } from '@/stores'
+import EditBox from '@/components/EditBox.vue'
 
 const store = useStore()
 const list = computed(() => store.lists)
-
-const toggle = ref(false)
+const currentEditTask = computed(() => store.currentEditTask)
 </script>
 
 <template>
@@ -31,25 +31,6 @@ const toggle = ref(false)
     </div>
 
     <!-- lightbox -->
-    <div v-if="toggle" class="fixed top-0 left-0 w-full h-full bg-slate-800 z-100 bg-opacity-70">
-      <div class="w-1/2 h-auto block relative mx-auto top-[15vh] bg-white py-8 px-12">
-        <div>
-          <input ref="target" type="text" class="w-full p-2 text-xl border mb-6">
-        </div>
-
-        <textarea
-          class="w-full h-[300px] p-3 overflow-x-hidden overflow-y-auto resize-none border"
-        />
-
-        <div class="text-right mt-4">
-          <button class="border bg-rose-500 text-white py-2 px-4 hover:bg-rose-700 mr-6">
-            刪除
-          </button>
-          <button class="border bg-slate-200 py-2 px-4 hover:bg-slate-400 hover:text-slate-100">
-            儲存送出
-          </button>
-        </div>
-      </div>
-    </div>
+    <EditBox v-if="currentEditTask?.id" />
   </div>
 </template>
