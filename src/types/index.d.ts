cf80c11e29f79789734e682c2fb12b47e5131730
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-01-05 10:00:58 +0800

    feat(types): define CurrentEditTask type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,22 +1,33 @@
 interface TaskItem {
+  /** 任務 id */
   id:string
+  /** 任務名稱 */
   title:string
+  /** 任務內容 */
   content:string
 }
 
 type Tasks = TaskItem[]
 
 interface TaskCard {
+  /** 卡片 id */
   id: string
+  /** 卡片名稱 */
   title: string
+  /** 任務列表 */
   tasks: Tasks
 }
 
 type TasksList = TaskCard[]
 
+interface CurrentEditTask extends TaskItem {
+  cardId: string
+}
+
 export {
   TaskCard,
   TaskItem,
   Tasks,
-  TasksList
+  TasksList,
+  CurrentEditTask
 }
